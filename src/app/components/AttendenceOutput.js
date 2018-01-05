5d10ff588f278954fc7c3f61b3b44a8991b555a5
Giora Guttsait <giora111@gmail.com>
2018-01-05 10:49:47 +0200

    Recomposed AttendenceOutput

--- src/app/components/AttendenceOutput.js
+++ src/app/components/AttendenceOutput.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import CopyToClipboard from 'react-copy-to-clipboard'
 import {Button, FormGroup, FormControl} from 'react-bootstrap'
+import {branch, renderComponent, mapProps, compose} from 'recompose'
 
 import isMobile from '../utils/platform'
 
@@ -9,34 +10,40 @@ const report1Output = soldiers =>
     .map(s => `*${s.name.trim()}:* ${s.attendence}`)
     .join('\n')
 
-const MobileOutput = ({soldiers}) => (
+const MobileOutput = ({text}) => (
   <FormGroup controlId="formControlsTextarea">
     <FormControl
       componentClass="textarea"
       placeholder="העתק דוח1"
-      value={report1Output(soldiers)}
+      value={text}
       readOnly
       onClick={e => e.target.select()}
     />
   </FormGroup>
 )
 
-const DesktopOutput = ({soldiers}) => {
-  const disabled = soldiers.length === 0
-  const bsStyle = disabled ? 'default' : 'primary'
-
-  return (
-    <CopyToClipboard text={report1Output(soldiers)} onCopy={() => console.log('copied')}>
-      <Button block {...{disabled, bsStyle}}>
-        העתק דוח1
-      </Button>
-    </CopyToClipboard>
-  )
-}
-
-const AttendenceOutput = ({soldiers}) => (isMobile()
-  ? <MobileOutput {...{soldiers}}/>
-  : <DesktopOutput {...{soldiers}}/>
+const DesktopOutput = ({text, bsStyle, disabled}) => (
+  <CopyToClipboard {...{text, onCopy: () => console.log('copied')}}>
+    <Button block {...{disabled, bsStyle}}>
+      העתק דוח1
+    </Button>
+  </CopyToClipboard>
 )
 
-export default AttendenceOutput
+const enhance = compose(
+  mapProps(({soldiers: {length}, soldiers}) => {
+    const disabled = length === 0
+    return {
+      disabled,
+      bsStyle: disabled ? 'default' : 'primary',
+      text: report1Output(soldiers),
+    }
+  }),
+  branch(
+    isMobile,
+    renderComponent(MobileOutput),
+    renderComponent(DesktopOutput),
+  )
+)
+
+export default enhance()
