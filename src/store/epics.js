7d6f20491895bcd56802b27fce3bb2cfad1b0085
Giora Guttsait <giora111@gmail.com>
2018-01-07 00:16:35 +0200

    Removed junk code from epics.js

--- src/store/epics.js
+++ src/store/epics.js
@@ -1,16 +1,4 @@
-// import {makeAsyncEpic} from 'redux-toolbelt-observable'
-// import {combineEpics} from 'redux-observable'
-// import {push} from 'react-router-redux'
-// import {Observable} from 'rxjs/Observable'
-// eslint-disable-next-line import/no-unresolved
-// import {empty} from 'rxjs/observable'
 import {take, switchMap} from 'rxjs/operator'
-// import {api} from 'shared/services'
-// import {
-//   ACTION_ASYNC_FAILURE_SUFFIX,
-//   ACTION_ASYNC_SUCCESS_SUFFIX,
-// } from 'redux-toolbelt'
-// import {error, success} from 'react-notification-system-redux'
 import * as actions from './actions'
 
 export const init = $action =>
@@ -18,6 +6,5 @@ export const init = $action =>
     .ofType(actions.init.TYPE)
     ::take(1)
     ::switchMap(() => {
-      console.log('put init code here and issue actions')
       return []
     })
