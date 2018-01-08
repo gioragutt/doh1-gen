cdf8a449ce1df130e6f558a4c7bc8e90d7fd4749
Giora Guttsait <giora111@gmail.com>
2018-01-08 03:06:53 +0200

    create wrappers to antd layout components

---
+++ src/shared/components/Layout.js
@@ -0,0 +1,28 @@
+import styled from 'react-emotion'
+import {Layout as ALayout} from 'antd'
+
+const {
+  Header: AHeader,
+  Content: AContent,
+  Footer: AFooter,
+  Sider: ASider,
+} = ALayout
+
+export const Layout = styled(ALayout)`
+  min-height: 100vh;
+`
+
+export const Footer = styled(AFooter)`
+  text-align: center;
+`
+
+export const Content = styled(AContent)`
+  margin: 0 16px;
+`
+
+export const Header = styled(AHeader)`
+  background: white;
+  padding: 0
+`
+
+export const Sider = styled(ASider)``
